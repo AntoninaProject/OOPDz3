74910f1d074fac1a38b331e924088352a7b34e5e
Antonina Petrova <ant2466@yandex.ru>
2023-07-20 19:19:41 +0300

    FamilyTreeIteratorispr

--- src/model/familyTree/FamilyTreeIterator.java
+++ src/model/familyTree/FamilyTreeIterator.java
@@ -3,7 +3,7 @@ package model.familyTree;
 import java.util.Iterator;
 import java.util.List;
 
-public class FamilyTreeIterator<T extends TreeNode> implements Iterator<T> {
+public class FamilyTreeIterator<T extends TreeNode<T>> implements Iterator<T> {
     private List<T> list;
     private int index;
 
