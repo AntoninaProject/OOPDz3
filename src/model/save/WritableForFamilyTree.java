72a4afc7ba6bda4fb46761a19e06ca32d829df21
Antonina Petrova <ant2466@yandex.ru>
2023-07-22 22:52:46 +0300

    WritableForFamilyTreeIspr

--- src/model/save/WritableForFamilyTree.java
+++ src/model/save/WritableForFamilyTree.java
@@ -2,7 +2,14 @@ package model.save;
 
 import java.io.IOException;
 
+import model.familyTree.FamilyTree;
+import model.human.Human;
+
 public interface WritableForFamilyTree {
     void writeObj(Object object) throws ClassNotFoundException, IOException;
     Object readObj() throws ClassNotFoundException, IOException;
+    static boolean save(FamilyTree<Human> activeTree) {
+        return false;
+    }
+    FamilyTree<Human> read();
 }
